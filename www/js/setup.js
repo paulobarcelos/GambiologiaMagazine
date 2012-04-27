d6ea8848073394b2241d5582d384c001df276178
Paulo Barcelos <paulo@azucrina.org>
2012-04-27 02:01:25 +0200

    Started saving the issues

--- www/js/setup.js
+++ www/js/setup.js
@@ -1,8 +1,19 @@
 // Our only global object
 var app = {
-	path : window.location.href.replace(/pages\/.*?$/, '').replace('index.html',''),
+	// this will change every time we swith page, we use to know the name of current apge
 	page : '',
-	inited : false
+	// app.path will always give us the root of our app
+	path : window.location.href.replace(/pages\/.*?$/, '').replace('index.html',''),
+	
+	// A few constants
+	server : "http://gambiologia.dev/",
+	issuesStructureRemotePath : "issues.json",
+	issuesStructureLocalPath : "issues.json",
+
+	// To following "constants" will be popuplates as soon as we can get a hold on them
+	permanentFileSystem: {},
+	permanentEntry: {},
+	messages : {}
 };
 
 // Setup some modules & plguins aliases
@@ -23,11 +34,6 @@ require.config({
 
 // There is some functionality in jQuery Mobile which is not App like, we fix this in both the CSS and JS
 $(document.body).live('pageinit', function (event, data){
-	if(!app.inited){
-		app.inited = true;
-
-		$.mobile.changePage(app.path + 'pages/sample/index.html');
-	}
 	$('.ui-footer').removeClass('slideup'); 
 	$('.ui-header').removeClass('slidedown');		
 	$('.ui-page-footer-fullscreen.ui-page-header-fullscreen .ui-content').css('padding-bottom',$('.ui-page-footer-fullscreen .ui-footer').height() + 11);
@@ -41,8 +47,7 @@ $(document.body).live('pagebeforeload', function (event, eventData){
 	$.get(eventData.absUrl, function(response){
 		var pageCollection = $(response);
 		app.page = pageCollection.attr('data-page');
-
-		//alert(pageCollection);	
+	
 		require([app.path + 'pages/' + app.page + '/' + app.page + '.js'], function(page){
 				$(document.body).append(pageCollection);
 				page.init(pageCollection, function(){
@@ -54,6 +59,61 @@ $(document.body).live('pagebeforeload', function (event, eventData){
 
 });
 
+// Making sure to keep old pages away form the dom
 $(document.body).live('pageshow', function (event, eventData){
 	eventData.prevPage.remove();
 });
+
+
+// Here is how we hook everything to the loading of the page
+$(window).load(function () {
+	// After the page is fully loaded, we wait for the device to get ready
+	document.addEventListener("deviceready", onDeviceReady, false);
+});
+
+// This will be called when cordova is ready
+var onDeviceReady = function(){
+
+	// ----------------- Step 1 
+
+	// This will be called when the filesystem is ready
+	var onFileSystemSuccess = function(fileSystem) {
+        app.permanentFileSystem = fileSystem;
+        app.permanentEntry = new DirectoryEntry({fullPath:  app.permanentFileSystem.root.fullPath});
+        // Goto to step 2
+        loadStrings();
+    }
+
+    // This will be called if the file system fails.
+    // Currently we can't do anything about that. :P
+    var onFileSystemFail = function(evt) {
+    	navigator.notification.alert(
+    		"Sorry, an error happened, but we don't really know how"
+    		+ "to handle it... here is the error message:"
+    		+ evt.target.error.code);
+    }
+
+    // Resquest the filesystem that evetually will kick in our chain initizalion
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess, onFileSystemFail);
+
+    // ----------------- Step 2 
+
+   	// Load some static data to the app
+    var loadStrings =  function(){
+    	require( ['i18n!' + app.path + 'static-strings/nls/messages.js' ], function( messages ){
+			app.messages = messages;
+
+			 // Goto to step 3
+        	onInitComplete();
+		});
+    }
+
+	// ----------------- Step 3 
+
+
+    // This will be called when all initialization is complete
+    // and we are ready to load our first page
+    var onInitComplete = function(){
+    	$.mobile.changePage(app.path + 'pages/start/index.html');
+    }
+}
--- www/pages/start/start.js
+++ www/pages/start/start.js
@@ -1,8 +1,9 @@
-define( ['pagecommon'], function( pagecommon ){
+define( ['pagecommon', 'util'], function( pagecommon, util ){
 	
-	// Top level vars, to store as soon as init is called
+	// Top level vars, to stored as soons as they become available
 	var page,
-	resolve;
+	resolve,
+	issuesData;
 
 	/**
 	* Called by the "manager", will provide a pointer to the current
@@ -10,28 +11,12 @@ define( ['pagecommon'], function( pagecommon ){
 	* soon as the page is ready to be displayed
 	**/
 	function init( _page, _resolve ){
+		// Sotre out top level vars
 		page = _page;
 		resolve = _resolve;
 
-		pagecommon.init(page,function(){
-			// First thing we do is to check if we have our issues
-			// data alredy stored. We do that by checking the a flag
-			// in the file storage.
-			var issuesStrucurePresent = window.localStorage.getItem("issuesStrucurePresent");
-			if(!issuesStrucurePresent){
-				// If we don't have it, we need to donwload it from the 
-				// remote server...
-
-				// Is there internet connection?
-				var networkState = navigator.network.connection.type;
-				if()
-
-
-			}
-
-
-			resolve.call();
-		});
+		// Init the common modules
+		pagecommon.init( page, afterCommon );
 	}
 
 	/**
@@ -39,7 +24,109 @@ define( ['pagecommon'], function( pagecommon ){
 	* the page.
 	**/
 	function afterCommon(){
-		
+		// -------------------- Step 1
+		/**
+		* Try to get a hold on the issues data
+		**/
+		var loadIssuesData = function(){
+			$.ajax({
+				url : app.permanentFileSystem.root.fullPath +'/' + app.issuesStructureLocalPath,
+				dataType: 'json',
+				success : onIssuesLoadSucess,
+				error : onIssuesLoadFail
+			});
+		}		
+
+		// --------- 1 - A
+		// If issues data can be loaded successfully from local storage
+		var onIssuesLoadSucess = function(data){
+			issuesData = data;
+
+			// Now that we have the data, we will create a directory for
+			// each issue. We first try to check if the first directory
+			// exists; if so we know earlier that they were already created
+			app.permanentFileSystem.root.getDirectory(
+				issuesData.issues[0].id,
+				{},
+				function(dir){
+					listIssues();
+				},
+				function(){
+					console.log('Error loading first directory');
+					createIssuesDirectories();
+				}
+
+			);
+
+			// Will create a directory for each issue and when done, move to the
+			// next step.
+			var createIssuesDirectories = function(){
+				// create a "lazy" listIssues function using our "trigger" util.
+				// the function will only be executed after it has been called
+				// for the exact number of time specified in the firt argument.
+				var lazyListIssues = util.trigger(issuesData.issues.length, listIssues, {});
+
+				for( var i = 0; i <  issuesData.issues.length; i++){					
+					// Retrieve an existing directory, or create it if it does not already exist
+					app.permanentFileSystem.root.getDirectory(
+						issuesData.issues[i].id, 
+						{create: true, exclusive: true}, 
+						function(dir){
+							lazyListIssues();
+						},
+						function(error){
+							console.log('Error at createIssuesDirectories', error);
+						}
+					);
+				}
+			}			
+		}
+
+		// --------- 1 - B
+		// If issues data was not found on local storage
+		var onIssuesLoadFail = function(){
+			// Is there internet connection?
+			var networkState = navigator.network.connection.type;
+			if( networkState != Connection.NONE || networkState != Connection.UNKNOWN ){
+				// Ok there is internet, let's load the file	
+				$.mobile.showPageLoadingMsg(); // show the loader			
+				var fileTransfer = new FileTransfer();
+				fileTransfer.download(
+					// URL
+					app.server + app.issuesStructureRemotePath,
+					// Download path
+					app.permanentFileSystem.root.fullPath + '/' + app.issuesStructureLocalPath,
+					// On success
+					function(entry) {
+						$.mobile.hidePageLoadingMsg(); // hide the loader
+						// Now that we have the file, let's load it
+						loadIssuesData();
+					},
+					// On error
+					function(error) {
+						$.mobile.hidePageLoadingMsg(); // hide the loader
+						navigator.notification.alert(app.messages.connectionError);
+						console.log("download error source " + error.source);
+						console.log("download error target " + error.target);
+						console.log("upload error code" + error.code);
+
+						// We don't have the file, but let's move to the next step anyway
+						listIssues();
+					}
+				);
+			}
+		}
+
+		loadIssuesData();
+	}
+
+	/**
+	* Show all the issues, or display the a message 
+	* in case we couldn't access the issues data
+	**/
+	function listIssues(){
+		console.log(issuesData.issues)
+		resolve.call();
 	}
 
 	return {
